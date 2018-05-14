ee8246f2933091498d207baca80d05303457d735
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-05-14 23:41:32 +0200

    Update detecta_bisiestos.py

--- detecta_bisiestos.py
+++ detecta_bisiestos.py
@@ -1,18 +1,27 @@
 from datetime import date
-from VALID import opt, ns
+from VALID import opt, ns, OKI
 import subprocess
 
+def año_val(n):
+    while n<1 or n>9999:
+        n=OKI(input("Año no válido: "))
+    return n
+    
 while True:
     print("DETECTA-BISIESTOS")
     print("¿Que información desea ver?")
     print("A)AÑOS BISIESTOS Y NO BISIESTOS")
     print("B)SOLO AÑOS BISIESTOS")
     print("C)SOLO AÑOS NO BISIESTOS")
+    rang=[]
     op=opt(input("Introduzca aquí su opción: "),["A","B","C"])
-    ran=input("Introduzca el rango de años separados por coma: ")
-    años=ran.split(",")
-    años.sort()
-    for i in range(int(años[0]),(int(años[1]))+1):
+    año1=año_val(OKI(input("Primer año del rango: ")))
+    rang.append(año1)
+    año2=año_val(OKI(input("Segundo año del rango: ")))
+    rang.append(año2)
+    rang.sort()
+    numan=0
+    for i in range(int(rang[0]),(int(rang[1]))+1):
         if i==9999:
             numdias=365
         else:
@@ -27,9 +36,14 @@ while True:
         else:
             if op==("B") and numdias==366:
                 print(i)
+                numan+=1
             elif op==("C") and numdias==365:
                 print(i)
-    conti=ns(input("¿Desa continuar: "))
+                numan+=1
+    if numan==0 and (op=="B" or op=="C"):
+        print("NO HAY AÑOS")
+    conti=ns(input("¿Desa continuar?: "))
     if conti==("n"):
         break
     subprocess.call(["cmd.exe","/C","cls"])
+            
