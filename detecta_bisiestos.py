92fd84f81e602d48f60d875219cc85291d703e34
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-05-18 10:02:58 +0200

    Update detecta_bisiestos.py

--- detecta_bisiestos.py
+++ detecta_bisiestos.py
@@ -7,9 +7,7 @@ def rang_año():
         rang=input("Escriba los años del rango separados por coma: ")
         defe=rang.split(",")
         try:
-            co=[]
-            co.append(int(defe[0]))
-            co.append(int(defe[1]))
+            co=[int(defe[0]),int(defe[1])]
             co.sort()
         except:
             continue
@@ -24,30 +22,46 @@ while True:
     print("A)AÑOS BISIESTOS Y NO BISIESTOS")
     print("B)SOLO AÑOS BISIESTOS")
     print("C)SOLO AÑOS NO BISIESTOS")
-    op=opt(input("Introduzca aquí su opción: "),["A","B","C"])
-    rang=rang_año()
-    numan=0
-    for i in range(int(rang[0]),(int(rang[1]))+1):
-        if i==9999:
+    print("D)CONSULTAR UN AÑO")
+    op=opt(input("Introduzca aquí su opción: "),["A","B","C","D"])
+    if op==("D"):
+        año=OKI(input("Indique año: "))
+        while año<1 or año>9999:
+            año=OKI(input("Año no válido: "))
+        if año==9999:
             numdias=365
         else:
-            D1=date(i,1,1)
-            D2=date(i+1,1,1)
+            D1=date(año,1,1)
+            D2=date(año+1,1,1)
             numdias=(abs(D2-D1).days)
-        if op==("A"):
-            if numdias==366:
-                print("El año",i,"es bisiesto")
+            if numdias==365:
+                print("El año",año,"no es bisiesto")
             else:
-                print("El año",i,"no es bisiesto")
-        else:
-            if op==("B") and numdias==366:
-                print(i)
-                numan+=1
-            elif op==("C") and numdias==365:
-                print(i)
-                numan+=1
-    if numan==0 and (op=="B" or op=="C"):
-        print("NO HAY AÑOS")
+                print("El año",año,"es bisiesto")
+    else:
+        rang=rang_año()
+        numan=0
+        for i in range(int(rang[0]),(int(rang[1]))+1):
+            if i==9999:
+                numdias=365
+            else:
+                D1=date(i,1,1)
+                D2=date(i+1,1,1)
+                numdias=(abs(D2-D1).days)
+            if op==("A"):
+                if numdias==366:
+                     print("El año",i,"es bisiesto")
+                else:
+                    print("El año",i,"no es bisiesto")
+            else:
+                if op==("B") and numdias==366:
+                    print(i)
+                    numan+=1
+                elif op==("C") and numdias==365:
+                    print(i)
+                    numan+=1
+        if numan==0 and (op=="B" or op=="C"):
+            print("NO HAY AÑOS")
     conti=ns(input("¿Desa continuar?: "))
     if conti==("n"):
         break
